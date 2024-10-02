ce69a6c6f705d70b09ee645aaf4152b3c96859d8
Mesbah Uddin Ahamed <mesbah@Mesbahs-MacBook-Pro.local>
2024-10-01 23:12:23 -0700

    Please

--- backend/db.js
+++ backend/db.js
@@ -1,16 +1,13 @@
 require('dotenv').config();
+const { Pool } = require("pg");
 
-// Access environment variables
-const {  DB_PASSWORD } = process.env;
-
-
-const Pool = require("pg").Pool;
 const pool = new Pool({
     host: "aws-0-ap-southeast-1.pooler.supabase.com",
     port: 6543,
     user: "postgres.crtqyfeppilazklquwvc",
     database: "postgres",
-    password: DB_PASSWORD,
+    password: process.env.DB_PASSWORD, 
 });
 
-module.exports = pool;
+
+module.exports = pool
