416dc8da1789ae94a85ab0412b16786f4f5e2637
Dipit Saha <sdipit099@gmail.com>
2024-10-05 10:57:48 +0600

    toatify remove pls

--- frontend/src/config/SERVER_URL.js
+++ frontend/src/config/SERVER_URL.js
@@ -1,6 +1,7 @@
 
-//const SERVER_URL = "http://localhost:8000";
+//const SERVER_URL = "http://localhost:8000";  
 
+// remove the comment from the above line and comment the below line to run the app locally
 
 const SERVER_URL = "https://exoreact-nasa-space-apps.onrender.com";
 
